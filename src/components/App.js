3c5cb98b986810d6d812dc62d262f04493a5ec0c
Olha Gruzglina <olga.zhmura@gmail.com>
2022-01-31 14:38:26 -0500

    commit tree 21f2cafe48324694122133239bcaa4e52691c06f

--- src/components/App.js
+++ src/components/App.js
@@ -5,14 +5,14 @@ import About from "./About";
 
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
-console.log(user);
+const {name, city, bio, color, links} = user;
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home name = {name} city = {city} color = {color}/>
+      <About bio = {bio} links = {links}/>
     </div>
   );
 }
